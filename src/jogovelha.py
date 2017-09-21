01b5bacfccd4406e58b411fee85e0b444e62c397
Michael Jordan <michael.jordan.java@gmail.com>
2017-09-20 21:34:18 -0300

    #8

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,27 +1,35 @@
 TAB = []
 
 def inicializar() :
- TAB.append(['.','.','.'])
- TAB.append(['.','.','.'])
- TAB.append(['.','.','.'])
+     TAB.append(['.','.','.'])
+     TAB.append(['.','X','.'])
+     TAB.append(['.','.','.'])
  
 def jogar(jogador, linha, coluna):
- if jogador !='X' and jogador != 'O':
- raise RuntimeError('Jogador inválido!')
- valores = list(range(0,3))
- if linha not in valores:
- raise RuntimeError('Linha inválida!')
- if coluna not in valores:
- raise RuntimeError('Coluna inválida!')
- TAB[linha][coluna] = jogador
+     if jogador !='X' and jogador != 'O':
+         raise RuntimeError('Jogador inválido!')
+
+     valores = list(range(0,3))
+
+     if linha not in valores:
+         raise RuntimeError('Linha inválida!')
+
+     if coluna not in valores:
+         raise RuntimeError('Coluna inválida!')
+
+     if TAB[linha][coluna] == 'X' or TAB[linha][coluna] == 'O':
+         raise RuntimeError('jogada inválida!')
+     else:
+         TAB[linha][coluna] = jogador
+        
  
 def tabuleiro():
- return TAB
+     return TAB
  
 def main():
- inicializar()
- jogar('X', 1, 1)
- print(tabuleiro())
+     inicializar()
+     jogar('X', 1, 1)
+     print(tabuleiro())
  
 if __name__ == "__main__":
- main()
+     main()
